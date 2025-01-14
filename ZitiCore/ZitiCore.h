9374923c80b3adbf83a69daa57268dec8d21d5ff
Nicholas Brunhart-Lupo <nicholasbl@icloud.com>
2025-01-14 08:23:15 -0700

    Initial Commit

---
+++ ZitiCore/ZitiCore.h
@@ -0,0 +1,18 @@
+//
+//  ZitiCore.h
+//  ZitiCore
+//
+//  Created by Nicholas Brunhart-Lupo on 1/14/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZitiCore.
+FOUNDATION_EXPORT double ZitiCoreVersionNumber;
+
+//! Project version string for ZitiCore.
+FOUNDATION_EXPORT const unsigned char ZitiCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZitiCore/PublicHeader.h>
+
+
---
+++ ZitiCoreTests/ZitiCoreTests.swift
@@ -0,0 +1,17 @@
+//
+//  ZitiCoreTests.swift
+//  ZitiCoreTests
+//
+//  Created by Nicholas Brunhart-Lupo on 1/14/25.
+//
+
+import Testing
+@testable import ZitiCore
+
+struct ZitiCoreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
