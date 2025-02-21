54bce0385a7a533c3ddb513192c1907289882857
Nicholas Brunhart-Lupo <nicholasbl@icloud.com>
2025-02-21 09:45:00 -0700

    Adding reconnect

--- ZitiCore/Communication.swift
+++ ZitiCore/Communication.swift
@@ -11,17 +11,24 @@ import SwiftCBOR
 import RealityKit
 import Starscream
 
-// REFACTOR! We are launching a task per message. this means tasks can and will be deferred
-// we need a task that just gets messages ready here
-// and another task on the main thread that reads the queue and handles them
+enum ConnectionState {
+    case connected
+    case connecting
+    case disconnected
+    case reconnecting
+}
 
 public class NoodlesCommunicator {
     var url: URL
     var socket : WebSocket!
     var queue = DispatchQueue(label: "gov.nrel.noodles.ziti")
-    var scene : (any RealityViewContentProtocol)!
     var decoder : MessageDecoder
     public var world : NoodlesWorld
+    
+    private var connection_state: ConnectionState = .disconnected
+    private var reconnect_attempts : Int = 0
+    private var max_reconnect_attempts : Int = 8
+    private var base_reconnect_delay : TimeInterval = 5
 
     public init(url: URL, world : NoodlesWorld) {
         print("Starting connection to \(url.host() ?? "UNKNOWN") at \(url.port ?? 50000)")
@@ -35,6 +42,19 @@ public class NoodlesCommunicator {
         socket.callbackQueue = queue
         socket.onEvent = self.on_recv_cb
         socket.connect()
+    }
+    
+    public func connect() {
+        guard connection_state == .disconnected || connection_state == .reconnecting else { return }
+        connection_state = .connecting
+        print("Opening socket...")
+        socket.connect()
+    }
+    
+    public func disconnect() {
+        connection_state = .disconnected
+        print("Closing socket...")
+        socket.disconnect()
     }
     
     public func send<T : NoodlesMessage>(msg: T) {
@@ -61,6 +81,8 @@ public class NoodlesCommunicator {
     }
     
     func on_message_data(data: Data) {
+        // we handle decoding in this thread to avoid creating lots of small tasks.
+        // normally this decode is quick
         let slice = data.withUnsafeBytes { (ptr: UnsafeRawBufferPointer) in
             //let buffer = UnsafeBufferPointer(start: ptr, count: data.count)
             return ArraySlice(ptr)
@@ -81,46 +103,102 @@ public class NoodlesCommunicator {
             print("websocket encountered an error")
         }
         
-        handle_no_server()
+        handle_abnormal_disconnection(code: CloseCode.protocolError.rawValue)
     }
     
-    func handle_no_server() {
+    func show_error_message() {
         DispatchQueue.main.async {
             self.world.root_entity.isEnabled = false
             self.world.error_entity.isEnabled = true
         }
     }
     
+    func show_content() {
+        DispatchQueue.main.async {
+            self.world.root_entity.isEnabled = true
+            self.world.error_entity.isEnabled = false
+        }
+    }
+    
+    func handle_abnormal_disconnection(code: UInt16) {
+        print("Abnormal socket termination.")
+        connection_state = .disconnected
+        socket.disconnect(closeCode: code)
+        attempt_reconnect()
+    }
+    
+    func attempt_reconnect() {
+        print("Attempting to reconnect")
+        show_error_message()
+        
+        guard reconnect_attempts < max_reconnect_attempts else {
+            print("Reached maximum reconnect attempts. Stopping.");
+            return;
+        }
+        
+        reconnect_attempts += 1
+        
+        let delay = base_reconnect_delay * pow( 2.0, Double(reconnect_attempts) )
+        
+        print("Reconnecting in \(delay) seconds...")
+        
+        connection_state = .reconnecting
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
+            self.connect()
+        }
+    }
+    
     func on_recv_cb(event: Starscream.WebSocketEvent) {
         switch event {
         case .connected(let headers):
-            //isConnected = true
+            connection_state = .connected
+            reconnect_attempts = 0
+            show_content()
             print("websocket is connected: \(headers)")
             self.send(msg: IntroductionMessage(client_name: "Swift Client"))
+            
         case .disconnected(let reason, let code):
-            //isConnected = false
             print("websocket is disconnected: \(reason) with code: \(code)")
+            if code != 1000 {
+                handle_abnormal_disconnection(code: CloseCode.goingAway.rawValue)
+            }
+            
+            
         case .text(let string):
             print("Received text: \(string)")
+            
         case .binary(let data):
-            //print("Received data: \(data.count)")
             on_message_data(data: data)
-        case .ping(_):
-            break
+            
+        case .ping(let content):
+            socket.write(pong: content ?? Data())
+            
         case .pong(_):
             break
-        case .viabilityChanged(_):
+            
+        case .viabilityChanged(let viable):
+            if !viable {
+                handle_abnormal_disconnection(code: CloseCode.goingAway.rawValue)
+            }
             break
-        case .reconnectSuggested(_):
+            
+        case .reconnectSuggested(let suggestion):
+            if suggestion {
+                handle_abnormal_disconnection(code: CloseCode.goingAway.rawValue)
+            }
             break
+            
         case .cancelled:
-            //isConnected = false
-            break;
+            print("Socket cancelled.")
+            handle_abnormal_disconnection(code: CloseCode.goingAway.rawValue)
+            break
+            
         case .error(let error):
-            //isConnected = false
             handle_ws_error(error)
+            
         case .peerClosed:
-            handle_no_server()
+            handle_abnormal_disconnection(code: CloseCode.goingAway.rawValue)
         }
     }
     
